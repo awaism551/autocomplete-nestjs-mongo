b6149cb8a8dc14dd4585fd9a862e64f8306f8cc3
Muhammad Awais <muhammad.awais4@afiniti.com>
2022-06-15 15:24:38 +0500

    removed commented code

--- src/app.service.ts
+++ src/app.service.ts
@@ -38,27 +38,4 @@ export class AppService {
     .sort(`${sort}`)
     .exec();
   }
-
-  // getSuggestions(query: string): Promise<City[]> {
-  //   console.log('in fun, query', query);
-  //   // return this.citiesRepository.findBy({
-  //   //   name: Like(`%${query}%`)
-  //   // });
-  //   return this.citiesRepository.find({
-  //     where: {
-  //       // "name": { "$regex": ".*a.*"}
-  //       // name: Like(`%${query}%`)
-  //       // name: Like('a')
-  //       // name: new RegExp(`^${query}`)
-  //       // name: Raw(alias => `${alias} ILIKE '%${query}%'`),
-  //       // name: "Alma" <= this works
-  //       // name: new RegExp(query, 'i').toString()
-  //       // $or: [{ name: new RegExp(query, 'i').toString() }],
-  //       // name: {pattern: '.*a.*',options: ''}
-  //       // name: { $eq: "Alma" }
-  //     }
-  //   });
-  //   // not assignable to type 'string | FindOperator<string>'.ts(2322)
-
-  // }
 }
